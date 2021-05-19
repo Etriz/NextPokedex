f237dbedda3d9331dfaba14d4d9785c01106260e
ryanp <ryanpanimation@gmail.com>
2021-05-19 00:16:24 -0500

    call api to get list of pokemon

---
+++ components/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Layout = ({ title, children }) => {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
